0c0c64edbc4b45fedb215c6d08e76353895e92c6
unknown <d.tatochenko@gmail.com>
2021-03-27 14:59:19 +0200

    qq

---
+++ task_4.js
@@ -0,0 +1,26 @@
+const countTotalSalary = function(employees) {
+    let salaries = Object.values(employees);
+    let total = 0;
+    for(let i of salaries){
+        total +=i;
+    }
+    return console.log(total)
+  };
+  /*
+   * Вызовы функции для проверки работоспособности твоей реализации.
+   */
+  console.log(countTotalSalary({})); // 0
+  console.log(
+    countTotalSalary({
+      mango: 100,
+      poly: 150,
+      alfred: 80,
+    }),
+  ); // 330
+  console.log(
+    countTotalSalary({
+      kiwi: 200,
+      lux: 50,
+      chelsy: 150,
+    }),
+  ); // 400
